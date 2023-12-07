b5e97f7f223649d572ac5c6d43f2ff9a7e7bf1c3
Ayush <kumarayush9694@gmail.com>
2023-12-07 19:44:41 +0530

    Week-1 Assignments completed.

---
+++ hard/todo-list.js
@@ -0,0 +1,53 @@
+/*
+  Implement a class `Todo` having below methods
+    - add(todo): adds todo to list of todos
+    - remove(indexOfTodo): remove todo from list of todos
+    - update(index, updatedTodo): update todo at given index
+    - getAll: returns all todos
+    - get(indexOfTodo): returns todo at given index
+    - clear: deletes all todos
+
+  Once you've implemented the logic, test your code by running
+*/
+
+class Todo {
+  constructor (){
+    this.todos=[];
+  }
+  add(todo){
+    this.todos.push(todo);
+  }
+  remove(index){
+    if(index>=0&&index<this.todos.length){
+      this.todos.splice(index,1)
+    }
+    // else {
+    //   console.error("Error!! Index not found.")
+    // }
+  }
+  update(index, updateTodo){
+    if(index>=0&&index<this.todos.length){
+      this.todos[index]= updateTodo;
+    }
+    // else {
+    //   console.error("Error!! Index not found.")
+    // }
+  }
+  getAll(){
+    return this.todos;
+  }
+  get(indexOfTodo){
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    } 
+    else {
+      // console.error("Error!! Index not found.");
+      return null;
+    }
+  }
+  clear(){
+    this.todos=[];
+  }
+}
+
+module.exports = Todo;
